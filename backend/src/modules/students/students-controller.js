8b18a2165377c14385be7855bb12a925fa6f0ce8
Peter Ott (Valeska) <ottpeter@tutanota.io>
2026-08-26 10:11:33 +0200

    adds handleGetAllStudents

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -2,12 +2,12 @@ const asyncHandler = require("express-async-handler");
 const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { userId, roleId, name } = req.query;
+    const students = await getAllStudents({ userId, roleId, name });
+    res.json({ students });
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
 
 });
 
